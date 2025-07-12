6495ee7d9e1a4cfbf74f6adb257114013f653893
db750 <betand04@nyu.edu>
2025-07-12 11:05:02 -0400

    Add files via upload

--- DNSServer.py
+++ DNSServer.py
@@ -97,7 +97,7 @@ dns_records = {
     
 'nyu.edu.': {
     dns.rdatatype.A: '192.168.1.106',
-    dns.rdatatype.TXT: (f'"{encrypted_value.decode("utf-8")}"',),
+    dns.rdatatype.TXT: ('"gAAAAABocnP5eetV8-LvXY9roW8zYTQ3alBzPccJy0g8LvrHIR56wP784HXCE5F57xopW-7Cfp4aPqlrOOwp-0uOXa6xqbcwTA=="',),
     dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
     dns.rdatatype.AAAA:'2001:0db8:85a3:0000:0000:8a2e:0373:7312',
     dns.rdatatype.NS: 'ns1.nyu.edu.',
